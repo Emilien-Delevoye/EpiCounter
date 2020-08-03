ece0e2fddd6008469cf3863c0fba3c82c5d0a44f
Emilien Delevoye <emilien.delevoye@gmail.com>
2020-08-03 16:35:37 +0200

    Update file creation at new day time

--- WebInterface/save_data.py
+++ WebInterface/save_data.py
@@ -24,10 +24,9 @@ class SaveData(Thread):
                         file.close()
                 except FileNotFoundError:
                     pass
-                    #Faire ce qu'il manque dans cette fonction, dans le cas d'un nouveau fichier
                     with open("data/" + datetime.now().strftime("%d-%m-%Y") + ".json", "w") as file:
-                        print("pouet")
-                        file.write(str(json.dumps(data, indent=1)))
+                        tmp = {datetime.now().strftime("%d/%m/%Y %H:%M:%S"): data}
+                        file.write(str(json.dumps(tmp, indent=1)))
                         file.close()
             elif datetime.now().second % 30 != 0 and current is True:
                 current = False
