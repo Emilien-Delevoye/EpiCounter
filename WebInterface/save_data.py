cc1f46c3acdc01fa7f0c3ad8fad460fb6a424a0a
Emilien Delevoye <emilien.delevoye@epitech.eu>
2020-08-10 02:02:08 +0200

    Add database reset at the end of the day

--- WebInterface/save_data.py
+++ WebInterface/save_data.py
@@ -11,10 +11,12 @@ class SaveData(Thread):
 
     def run(self):
         current = False
+        reset = False
         while True:
             date = datetime.now()
             if date.second % 30 == 0 and current is False and (date.hour >= 8 or date.hour < 2):
                 current = True
+                reset = False
                 data = self.server.get_data()
                 try:
                     with open("data/" + get_file_name(), "r") as file:
@@ -32,3 +34,12 @@ class SaveData(Thread):
                         file.close()
             elif date.second % 30 != 0 and current is True:
                 current = False
+            if date.hour == 2 and reset is False:
+                reset = True
+                for i in self.server.database:
+                    for j in self.server.database[i]:
+                        if j != "total":
+                            self.server.database[i][j] = [0, 0]
+                        else:
+                            self.server.database[i][j] = 0
+                print("Success: Reset database")
