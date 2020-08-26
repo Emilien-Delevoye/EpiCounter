99d978fe20994a266c16e83e1ab59f168462418c
Cyprien <cyprien.ricque@epitech.eu>
2020-08-26 23:47:49 +0200

    add automatic db / table creation in dockerfile

---
+++ db/init.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE epicounter_db
+
+\c epicount_db
+
+CREATE TABLE count (
+    id INTEGER NOT NULL,
+    update INTEGER NOT NULL,
+    room VARCHAR (200) NOT NULL,
+    door VARCHAR (200) NOT NULL,
+    total INTEGER NOT NULL,
+    total_raw INTEGER,
+    date_created TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
+)
